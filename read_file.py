f824395d3d44f490e9b04962c8217ae5c41eebd3
ahmadhaghbin <ahmad.haghbin@yahoo.com>
2019-04-30 13:07:32 +0200

    up

---
+++ read_file.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_excel_data(file_name):
+    data_frame = pd.read_excel(file_name)
+    return data_frame
+
+
+if __name__ == '__main__':                 #read phython files
+    file_name = "WindSpeed.xlsx"
+    data_frame = read_excel_data(file_name)
+#    print(data)
+    wind_speed = data_frame ['Wind Speed'].values[1:]  #extract the values from first column
+    plt.plot(wind_speed) #plot command
+    plt.xlabel('Time [s]')
+    plt.ylabel('Wind Speed [m/s]')
+    plt.title('Wind Speed Profile')
+    plt.ylim (0,max(wind_speed)+5)
+    plt.xlim (0,len(wind_speed))
+    plt.show()          #print the figure
+
+    #print(wind_speed)
+    #columns = list(data.columns)
+    # wind_speed = list(data["Wind Speed"])
+#
+#     data2 = data.loc[14:6,"Wind Speed":"wd68"].values
+#     print(data2)
